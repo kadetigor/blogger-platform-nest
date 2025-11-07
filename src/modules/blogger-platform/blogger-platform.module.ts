098088a2a3cf8fc28fd97160ebefdf77d2507e03
kadetigor <73181723+kadetigor@users.noreply.github.com>
2025-11-07 12:52:12 +0100

    nn

--- src/modules/blogger-platform/blogger-platform.module.ts
+++ src/modules/blogger-platform/blogger-platform.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BlogsModule } from './blogs/blogs.module';
 import { PostsModule } from './posts/posts.module';
-import { QuizModule } from '../quiz/quiz.module';
 
 @Module({
   imports: [BlogsModule, PostsModule],
